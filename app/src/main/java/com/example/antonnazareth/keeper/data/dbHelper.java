03021ba862f2c638a7458210e0fd82902e61d8f2
antonze <antonze@users.noreply.github.com>
2015-08-23 11:42:21 +0100

    all of my pretty ui changes

--- app/src/main/java/com/example/antonnazareth/keeper/data/dbHelper.java
+++ app/src/main/java/com/example/antonnazareth/keeper/data/dbHelper.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.antonnazareth.keeper.data;
+
+//import com.example.antonnazareth.keeper.data.KeeperContract.Team2UserEntry;
 
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 
 /**
  * Manages a local database for weather data.
@@ -14,7 +30,7 @@ public class dbHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
     private static final int DATABASE_VERSION = 2;
 
-    static final String DATABASE_NAME = "weather.db";
+    public static final String DATABASE_NAME = "keeper.db";
 
     public dbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,41 +40,41 @@ public class dbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         // Create a table to hold locations.  A location consists of the string supplied in the
         // location setting, the city name, and the latitude and longitude
-        final String SQL_CREATE_USER_TABLE = "CREATE TABLE " + "-UserTable" + " (" +
-                "-ID" + "INTEGER PRIMARY KEY,-Name TEXT UNIQUE NOT NULL" + ");";
+        final String SQL_CREATE_USER_TABLE = "CREATE TABLE " + KeeperContract.UserEntry.TABLE_NAME + " (" +
+                KeeperContract.UserEntry.COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                KeeperContract.UserEntry.COLUMN_FIRST_NAME + " TEXT NOT NULL, " +
+                KeeperContract.UserEntry.COLUMN_LAST_NAME + " TEXT NOT NULL, " +
+                KeeperContract.UserEntry.COLUMN_FULL_NAME + " TEXT UNIQUE NOT NULL, " +
+                KeeperContract.UserEntry.COLUMN_NICKNAME + " REAL NOT NULL " +
+                " );";
 
-//        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
-//                // Why AutoIncrement here, and not above?
-//                // Unique keys will be auto-generated in either case.  But for weather
-//                // forecasting, it's reasonable to assume the user will want information
-//                // for a certain date and all dates *following*, so the forecast data
-//                // should be sorted accordingly.
-//                WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+        final String SQL_CREATE_TEAM_TABLE = "CREATE TABLE " + KeeperContract.TeamEntry.TABLE_NAME + " (" +
+
+                KeeperContract.TeamEntry.COLUMN_TEAM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KeeperContract.TeamEntry.COLUMN_TEAM_NAME + " TEXT UNIQUE NOT NULL " +
+                " );";
+
+
+//        final String SQL_CREATE_TEAM2USER_TABLE = "CREATE TABLE " + Team2UserEntry.TABLE_NAME + " (" +
+//
+//                KeeperContract.Team2UserEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
 //
 //                // the ID of the location entry associated with this weather data
-//                WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
-//                WeatherEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
-//                WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
-//                WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL," +
-//
-//                WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
-//                WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
-//
-//                WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
-//                WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
-//                WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
-//                WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
+//                KeeperContract.TeamEntry.COLUMN_TEAM_NAME + " TEXT UNIQUE NOT NULL, " +
 //
 //                // Set up the location column as a foreign key to location table.
-//                " FOREIGN KEY (" + WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " +
-//                LocationEntry.TABLE_NAME + " (" + LocationEntry._ID + "), " +
+//                " FOREIGN KEY (" + KeeperContract.Team2UserEntry.COLUMN_USER_KEY + ") REFERENCES " +
+//                KeeperContract.UserEntry.TABLE_NAME + " (" + KeeperContract.UserEntry.COLUMN_USER_ID + "), " +
 //
-//                // To assure the application have just one weather entry per day
-//                // per location, it's created a UNIQUE constraint with REPLACE strategy
-//                " UNIQUE (" + WeatherEntry.COLUMN_DATE + ", " +
-//                WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
+//                " FOREIGN KEY (" + KeeperContract.Team2UserEntry.COLUMN_TEAM_KEY + ") REFERENCES " +
+//                KeeperContract.TeamEntry.TABLE_NAME + " (" + KeeperContract.TeamEntry.COLUMN_TEAM_ID +
+//                " );";
 
         sqLiteDatabase.execSQL(SQL_CREATE_USER_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_TEAM_TABLE);
+//        sqLiteDatabase.execSQL(SQL_CREATE_TEAM2USER_TABLE);
+
+
     }
 
     @Override
@@ -69,7 +85,10 @@ public class dbHelper extends SQLiteOpenHelper {
         // It does NOT depend on the version number for your application.
         // If you want to update the schema without wiping data, commenting out the next 2 lines
         // should be your top priority before modifying this method.
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + "-UserTable");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + KeeperContract.UserEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + KeeperContract.TeamEntry.TABLE_NAME);
+        //sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Team2UserEntry.TABLE_NAME);
+
         onCreate(sqLiteDatabase);
     }
 }
